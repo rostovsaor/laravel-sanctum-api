b7349e8c8f82ac2c7ad67ef8fe16a281b4e0c8e3
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-05-13 21:45:54 +0200

    chore: product-controller and resource routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function() {
-    return 'products';
-});
+Route::resource('products', ProductController::class);
+
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+// Route::get('/products', [ProductController::class, 'index']);
+
+// Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
