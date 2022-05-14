4e8bb553cb8ce45018aabb3ab0175a467a2f942b
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-05-14 07:33:15 +0200

    chore: make non-get routes protected by auth

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,20 @@ use App\Http\Controllers\ProductController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::resource('products', ProductController::class);
-
+// Route::resource('products', ProductController::class);
+// Public Routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+
+// Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+});
 
 // Route::get('/products', [ProductController::class, 'index']);
 
