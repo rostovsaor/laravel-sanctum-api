cdd37076821f66be8bf4a87261ca28264985b0d3
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-05-14 08:21:17 +0200

    feat: login with sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
 /*
@@ -16,6 +17,8 @@ use App\Http\Controllers\ProductController;
 */
 // Route::resource('products', ProductController::class);
 // Public Routes
+
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
